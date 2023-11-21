64db4b7bd34b442417db9dfaf93c99954fec4d92
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-21 22:20:15 +0200

    Update user model relations and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many  :expenses, dependent: :destroy, foreign_key: :author_id
+  has_many  :categories, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :name, presence: true, length: { minimum: 2 }
 end
