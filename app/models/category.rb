2f607cabb20409a934cf4f39a7e0aa092a2e957a
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-21 22:19:55 +0200

    Update category model relations and validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :expenses, through: :category_expenses
+
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :icon, presence: true
 end
