2f75b2eb48fb87770b09be43b8af266fef4a3a6e
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-21 22:20:06 +0200

    Update expenses model relations and validation

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,8 @@
 class Expense < ApplicationRecord
   belongs_to :author
+  has_many :categories, through: :category_expenses
+
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :author, presence: true
 end
