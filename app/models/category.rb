431652f3d5f0227ab1ecee909f5bf429361f558d
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-22 04:40:31 +0200

    Add total_expenses method to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,12 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :category_expenses, dependent: :destroy
   has_many :expenses, through: :category_expenses
 
   validates :name, presence: true, length: { minimum: 2 }
   validates :icon, presence: true
+
+  def total_expenses
+    expenses.sum(:amount)
+  end
 end
