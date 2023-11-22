e80a6329f176ca98a20dd2291ae33b492a3c7c79
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-22 04:41:28 +0200

    Add registrations controller to devise_for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   resources :expenses
   resources :categories
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
