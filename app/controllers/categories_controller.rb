b498c71390f56c2568dc904e96cbe150b585bb35
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-24 22:19:54 +0200

    Remove unnecessary actions from CategoriesController

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,21 +1,13 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[show edit update destroy]
-
   # GET /categories or /categories.json
   def index
     @categories = current_user.categories
   end
 
-  # GET /categories/1 or /categories/1.json
-  def show; end
-
   # GET /categories/new
   def new
     @category = Category.new
   end
-
-  # GET /categories/1/edit
-  def edit; end
 
   # POST /categories or /categories.json
   def create
@@ -25,7 +17,7 @@ class CategoriesController < ApplicationController
     respond_to do |format|
       if @category.save
         flash[:success] = 'Category was successfully created!'
-        format.html { redirect_to category_url(@category) }
+        format.html { redirect_to :root }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,35 +26,7 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /categories/1 or /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params, user: current_user)
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /categories/1 or /categories/1.json
-  def destroy
-    @category.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_category
-    @category = Category.find(params[:id])
-  end
 
   # Only allow a list of trusted parameters through.
   def category_params
